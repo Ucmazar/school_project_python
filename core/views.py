975154b1c4adea6522b7efaea6bcd2646e771452
Fahim Jan <ucmazar@gmail.com>
2025-05-19 12:05:07 +0430

    add all docs

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import UserProfile
 
-# Create your views here.
+def user_list(request):
+    users = UserProfile.objects.all()
+    return render(request, 'core/user_list.html', {'users': users})
